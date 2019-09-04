eb31e3a1f252af5516f0e7f1d0fd8db35daaae56
Romina Delgado <rominadelgado_@hotmail.com>
2019-09-04 16:33:26 -0300

    .

--- app/src/main/java/com/example/MovieNote/NuevaAnotacion.java
+++ app/src/main/java/com/example/MovieNote/NuevaAnotacion.java
@@ -2,18 +2,21 @@ package com.example.MovieNote;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import android.content.Intent;
 
 import com.example.MovieNote.Entidades.ConexionSQLiteHelper;
+import com.example.MovieNote.Utilidades.Utilidades;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-
+import android.widget.Toast;
 
 
 public class NuevaAnotacion extends AppCompatActivity
@@ -31,36 +34,32 @@ public class NuevaAnotacion extends AppCompatActivity
         campoTitulo= (EditText) findViewById(R.id.campoTitulo);
         campoFecha=(EditText) findViewById(R.id.campoFecha);
 
-        FloatingActionButton floatingActionButton_guardar = findViewById(R.id.floatingActionButton_guardar);
-        floatingActionButton_guardar.setOnClickListener(new View.OnClickListener() {
+
+        Button button_registrar =findViewById(R.id.button_registrar);
+        button_registrar.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View view) {
-
-                    //AnotacionSimple anotacion = new AnotacionSimple(getText(R.id.editText_tituloPelicula).toString());
-                    //logica.guardar(anotacion);
-
-                    //floating action button
-                    Snackbar.make(view, "", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-
+               if (   ((EditText) findViewById(R.id.campoTitulo)).getText().toString() ==""
+                        &&
+                       ((EditText) findViewById(R.id.campoFecha)).getText().toString()==""
+               )
+                    Toast.makeText(getApplicationContext(),"No completo los campos correspondientes.",Toast.LENGTH_SHORT).show();
+                else
                     registrarAnotacion();
-
-                    // Vinculo el botón con la actividad NuevaAnotacion
-                    Intent miIntentButtonGuardar = new Intent(NuevaAnotacion.this, MainActivity.class);
-                    startActivity(miIntentButtonGuardar);
-                    finish();
-
-
-
-
             }
         });
+
     }
 
     private void registrarAnotacion()
     {
         ConexionSQLiteHelper conn = new ConexionSQLiteHelper(this, "db anotaciones",null, 1);
         SQLiteDatabase db=conn.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Utilidades.CAMPO_TITULO,campoTitulo.getText().toString());
+        values.put(Utilidades.CAMPO_FECHA,campoFecha.getText().toString());
 
     }
 
