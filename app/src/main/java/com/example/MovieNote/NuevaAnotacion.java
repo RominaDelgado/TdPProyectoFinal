1add3f5ca1a0dbd5fb5325cb733020eaaaf26ff7
Romina Delgado <rominadelgado_@hotmail.com>
2019-09-04 16:52:45 -0300

    modificaciones en NuevaAnotacion

--- app/src/main/java/com/example/MovieNote/NuevaAnotacion.java
+++ app/src/main/java/com/example/MovieNote/NuevaAnotacion.java
@@ -35,18 +35,28 @@ public class NuevaAnotacion extends AppCompatActivity
         campoFecha=(EditText) findViewById(R.id.campoFecha);
 
 
-        Button button_registrar =findViewById(R.id.button_registrar);
+        Button button_registrar =findViewById(R.id.button_guardar);
         button_registrar.setOnClickListener(new View.OnClickListener()
         {
+            // Al hacer click en el botón Guardar:
+            // -Si los campos estan vacios NO guardara nada y mostrara mensaje.
+            // -Si los campos estan completos, se registrara la anotación y se volvera a
+            //   pantalla principal donde esta la lista de anotaciones.
             @Override
             public void onClick(View view) {
                if (   ((EditText) findViewById(R.id.campoTitulo)).getText().toString() ==""
                         &&
-                       ((EditText) findViewById(R.id.campoFecha)).getText().toString()==""
-               )
-                    Toast.makeText(getApplicationContext(),"No completo los campos correspondientes.",Toast.LENGTH_SHORT).show();
+                       ((EditText) findViewById(R.id.campoFecha)).getText().toString()=="")
+
+                     Toast.makeText(getApplicationContext(),"No completo los campos correspondientes.",Toast.LENGTH_SHORT).show();
                 else
-                    registrarAnotacion();
+                    {
+                     registrarAnotacion();
+                        // Vinculo el botón con la actividad NuevaAnotacion
+                        Intent miIntentVolverPrincipal = new Intent(NuevaAnotacion.this, MainActivity.class);
+                        startActivity(miIntentVolverPrincipal);
+                        finish();
+                    }
             }
         });
 
@@ -60,7 +70,6 @@ public class NuevaAnotacion extends AppCompatActivity
         ContentValues values = new ContentValues();
         values.put(Utilidades.CAMPO_TITULO,campoTitulo.getText().toString());
         values.put(Utilidades.CAMPO_FECHA,campoFecha.getText().toString());
-
     }
 
 }
