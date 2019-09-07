520d2769773040fb95fe0ebc37021efbf04aedf5
Romina Delgado <rominadelgado_@hotmail.com>
2019-09-07 01:27:33 -0300

    corrección en Utilidades

--- app/src/main/java/com/example/MovieNote/Utilidades/Utilidades.java
+++ app/src/main/java/com/example/MovieNote/Utilidades/Utilidades.java
@@ -6,5 +6,5 @@ public class Utilidades
     public static final String CAMPO_TITULO="titulo";
     public static final String CAMPO_FECHA="fecha";
 
-    public static final String CREAR_TABLA_ANOTACION="CREATE TABLE"+TABLA_ANOTACION+"("+CAMPO_TITULO+"STRING,"+CAMPO_FECHA+"STRING)";
+    public static final String CREAR_TABLA_ANOTACION="CREATE TABLE anotaciones("+CAMPO_TITULO+"STRING,"+CAMPO_FECHA+"STRING)";
 }
